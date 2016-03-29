a72469f5db759b2c7f1d4d736ae148b95f1200e5
krentin018 <krentin.naidoo18@gmail.com>
2016-03-29 22:07:27 +0200

    Q3.1  3.2

--- Question3.cpp
+++ Question3.cpp
@@ -17,11 +17,43 @@ using namespace std;
 //     either order) and return a fraction.  
 // 3.3 create a driver program to test your overloaded > operators and your overloaded 
 
+
 class Fraction {
 private:
 	int num;				// numerator;
 	int denom;				// denominator;
 public:
-	Fraction(int n, int d) : num(n), denum(d) { };
+	Fraction(int n, int d) : num(n), denom(d) { };
 	void print() { cout << num << "/" << denom; };
+	friend bool operator>(Fraction f1, Fraction f2);
+	friend Fraction add(Fraction f1, int val);
+	friend Fraction add(int val, Fraction f1);
 };
+
+bool operator>(Fraction f1, Fraction f2)
+{
+	int num1, num2;
+	num1 = f1.num*f2.denom;
+	num2 = f2.num*f1.denom;
+	if (num1 > num2)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+Fraction add(Fraction f1, int val)
+{
+	f1.num += val*f1.denom;
+	return f1;
+}
+
+Fraction add(int val,Fraction f1)
+{
+	f1.num += val*f1.denom;
+	return f1;
+}
+
